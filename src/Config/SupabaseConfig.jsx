b0bd2cd866983c499c964769f5b1a1bddc66a5b5
Shoaib Raza <hafizshoaibraza180@gmail.com>
2025-02-06 12:18:06 +0500

    Loader Added

--- src/Config/SupabaseConfig.jsx
+++ src/Config/SupabaseConfig.jsx
@@ -1,11 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// const supabaseUrl = 'https://kzhxjaewpxxvbyaoqjyr.supabase.co';
-// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6aHhqYWV3cHh4dmJ5YW9xanlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2ODc2MzQsImV4cCI6MjA1NDI2MzYzNH0.8fpGSEETo6AZIdbU_m6Piod0VhM0zFQJy6jxyH08sEA';
-// export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 
-
-const supabaseUrl = "https://kzhxjaewpxxvbyaoqjyr.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6aHhqYWV3cHh4dmJ5YW9xanlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2ODc2MzQsImV4cCI6MjA1NDI2MzYzNH0.8fpGSEETo6AZIdbU_m6Piod0VhM0zFQJy6jxyH08sEA";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
